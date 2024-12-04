18bcaa5e0214b243be5f5953b8a4784d9db7f0c8
YodsawatPS <yodsawatphonsuk5449@gmail.com>
2024-12-04 11:25:40 +0700

    Labsheet02

---
+++ Labsheet02/src/Lab203.java
@@ -0,0 +1,25 @@
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+public class Lab203 {
+	public static void main(String[] args) {
+		DecimalFormat frm = new DecimalFormat("##.0");
+		double weight = Double.parseDouble(JOptionPane.showInputDialog("Input Weight :"));
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Input height :"));
+		double Meterheight = (height / 100);
+		double BMI = weight / (Meterheight * Meterheight);
+		if (BMI < 18.5) {
+			JOptionPane.showMessageDialog(null, "BMI =" + frm.format(BMI) + "\nYou're Underweight", "BMI",
+					JOptionPane.WARNING_MESSAGE);
+		} else if (BMI > 18.5 && BMI < 24.9) {
+			JOptionPane.showMessageDialog(null, "BMI =" + frm.format(BMI) + "\nYou're Normal-weight", "BMI",
+					JOptionPane.WARNING_MESSAGE);
+		} else if (BMI > 25.0 && BMI < 29.9) {
+			JOptionPane.showMessageDialog(null, "BMI =" + frm.format(BMI) + "\nYou're Overweight", "BMI",
+					JOptionPane.WARNING_MESSAGE);
+		} else if (BMI > 30.0) {
+			JOptionPane.showMessageDialog(null, "BMI =" + frm.format(BMI) + "\nYou're Obesity", "BMI",
+					JOptionPane.WARNING_MESSAGE);
+		}
+	}
+}
