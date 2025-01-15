eaff532b44a5c7ccf502ef266de6660e3724f989
YodsawatPS <yodsawatphonsuk5449@gmail.com>
2025-01-15 11:03:16 +0700

    LabSheet06_03

---
+++ LabSheet06/src/TestProduct.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class TestProduct {
+ 
+	public static void main(String[] args) {
+		
+		Product Pd= new Product();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter product Name: ");
+		String ProductName = scan.nextLine();
+		System.out.print("Enter product price: ");
+		double BeforeVat = scan.nextDouble();
+		System.out.print("Enter VAT rate (%): ");
+		double AfterVat = scan.nextDouble();	
+		Pd.setProductDetails(ProductName, BeforeVat, AfterVat);
+		System.out.println();
+		System.out.println("Product Details : ");
+		Pd.displayProductDetails();
+		scan.close();
+	}
+ 
+}
+ 
+ 
