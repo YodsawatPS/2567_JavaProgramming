41f54b3cfa506b35e8ea865f7d2258a5fb40c183
YodsawatPS <yodsawatphonsuk5449@gmail.com>
2025-01-22 11:17:11 +0700

    HomeWorkLabSheet06_4,5

---
+++ LabSheet06/src/TestLoanCalculator.java
@@ -0,0 +1,13 @@
+
+public class TestLoanCalculator {
+
+	public static void main(String[] args) {		
+
+		LoanCalculator loan = new LoanCalculator();
+
+		loan.setLoanDetails("Laptop", 50000, 5, 2);
+
+		loan.displayLoanDetails();
+	}
+
+}
