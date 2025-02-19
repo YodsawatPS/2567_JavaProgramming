28997cb8f194f6676f91c507e192cfc3ad874c80
YodsawatPS <yodsawatphonsuk5449@gmail.com>
2025-02-19 09:57:09 +0700

    HomeWorkLabSheet09

---
+++ LabSheet09/src/Shop100Baht.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+
+public class Shop100Baht {
+	public static void main(String[] args) {
+		Product sold = new Product();
+		int response = JOptionPane.showConfirmDialog(null, "Is this program run on Pattanakarn Branch?",
+				"Branch Selection", JOptionPane.YES_NO_OPTION);
+		if (response == JOptionPane.YES_OPTION) {
+			sold = new PattanakarnBranch();
+		} else {
+			sold = new Product();
+		}
+		int number = Integer.parseInt(JOptionPane.showInputDialog("Input the number of product: "));
+		sold.setUnit(number);
+		JOptionPane.showMessageDialog(null, sold);
+
+	}
+}
